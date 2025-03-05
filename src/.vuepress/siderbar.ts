4288a007d349fcade66e9422aef7fe2c7dc58c0c
zyl <870138612@qq.com>
2025-03-06 01:24:19 +0800

    添加目录

--- src/.vuepress/siderbar.ts
+++ src/.vuepress/siderbar.ts
@@ -1,13 +1,14 @@
 import {sidebar} from "vuepress-theme-hope";
 
 export const zhSidebar = sidebar({
-  "/": [
-    {
-      collapsible: true,
-      text: "openstack",
-      icon: "openstack",
-      prefix: "openstack/",
-      children: "structure",
-    }
-  ],
+    "/": [
+        {
+            collapsible: true,
+            text: "openstack",
+            icon: "openstack",
+            prefix: "openstack/",
+            children: "structure",
+        },
+        "intro.md"
+    ],
 });
