83949ee22f0b8e5f7ac0bdb504d6441c4fcd2e67
zyl <870138612@qq.com>
2025-03-10 00:41:26 +0800

    添加oslo_messaging走读
    添加nova.service.Service类走读
    
    添加目录

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import {defineUserConfig} from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-
+    dest:"./docs",
     base: "/",
     head: [
         ["meta", {name: "robots", content: "all"}],
